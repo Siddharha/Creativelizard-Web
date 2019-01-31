60bfc239495ff880c565b5b61b7852fa33b09c52
siddhartha <Phantom1650763>
2019-01-31 13:06:52 +0530

    sdfgsdfgh

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-// $( document ).ready(function() {
-//     $("#menu-start").click(function() {
-//         //alert('test');
-//         // $("#main-section").load("./pages/home.html");
-        
-//     });
-// });
+var page = 0;
 
-function clkHome(){
-    document.getElementById("main-section").innerHTML=
-    '<object type="text/html" data="./pages/home.html" id="id_home"></object>';
-    manageMenuStyle();
+$( document ).ready(function() {
+    var item = $("ul.menu").children()[0];
+    $("ul.menu").find( item).css( "background-color", "green" );
+    $("ul.menu").find( item).css( "color", "white" );
+    document.getElementById("main-section").innerHTML= '<object type="text/html" data="./pages/home_page/home.html" id="id_home"></object>';
+});
+function clkMnu(position){
+
+    switch (position) {
+        case 0:
+            page = '<object type="text/html" data="./pages/home_page/home.html" id="id_home"></object>';
+            break;
+        case 1:
+            page = '<object type="text/html" data="./pages/products_page/products.html" id="id_products"></object>';
+            break;
+        default:
+            break;
+    }
+    document.getElementById("main-section").innerHTML= page;
+    manageMenuStyle(position);
 }
 
-function manageMenuStyle(){
+function manageMenuStyle(position){
     var n = $("ul.menu").children().length;
     console.log(n);
     
     for (let index = 0; index < n; index++) {
-        //const element = array[index];
-        
+        $("ul.menu").children().css( "background-color", "rgb(70, 70, 70)" );
+        $("ul.menu").children().css( "color", "rgb(189, 189, 189)" );
     }
-    $("menu-start").css("background-color") = "green";
-    //$("menu-start").style.color = "white";
+    var item = $("ul.menu").children()[position];
+    $("ul.menu").find( item).css( "background-color", "green" );
+    $("ul.menu").find( item).css( "color", "white" );
 }
